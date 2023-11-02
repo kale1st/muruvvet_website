efb5fec4aa0e34ae1805f4b317609a05047a37cc
azizkale <azizkale@hotmail.com>
2023-11-02 17:57:33 +0100

    swipper works but not well

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,32 +1,34 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import Swiper from 'swiper';
+declare var $: any;
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
-  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
+export class ArticleComponent implements AfterViewInit {
 
   ngAfterViewInit() {
-    this.initSwiper();
+    $(document).ready(function () {
+
+      new Swiper('.testimonials-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true
+        }
+      })
+
+    });
+
   }
 
-  private initSwiper() {
-    new Swiper(this.swiperContainer.nativeElement, {
-      speed: 600,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-      slidesPerView: 'auto',
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-      },
-    });
-  }
 }
