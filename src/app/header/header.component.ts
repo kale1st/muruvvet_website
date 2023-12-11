3e4b376370ce97ec1be354e468e5f98a86faeb31
azizkale <azizkale@hotmail.com>
2023-12-11 07:19:23 +0100

    fix/mobile menu toggle fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +7,34 @@ import { Component } from '@angular/core';
 
 })
 export class HeaderComponent {
-  isActive: string | null = null;
+  @ViewChild('header') header: ElementRef | any;
+  @ViewChild('body') body: ElementRef | any;
+
+  isActive: boolean = true;//side menu active?
+
+  constructor(private renderer: Renderer2) { }
+
+  // <div #body class="mobile-nav-active">
+  // <i class="bi mobile-nav-toggle d-lg-none bi-x"></i>
+
+  //   <i
+  //   class="bi mobile-nav-toggle d-lg-none bi-list"
+  //   (click)="toggleMobileNav()"
+  // ></i>
 
   toggleMobileNav(): void {
-    // Add logic to toggle mobile navigation
+    if (this.isActive) {
+      this.renderer.removeClass(this.header.nativeElement, 'bi-list');
+      this.renderer.addClass(this.header.nativeElement, 'bi-x');
+      this.renderer.addClass(this.body.nativeElement, 'mobile-nav-active');
+      this.isActive = false;
+    }
+    else {
+      this.renderer.removeClass(this.header.nativeElement, 'bi-x');
+      this.renderer.addClass(this.header.nativeElement, 'bi-list');
+      this.renderer.removeClass(this.body.nativeElement, 'mobile-nav-active');
+      this.isActive = true;
+    }
   }
 
-  setActive(section: string): void {
-    this.isActive = section;
-  }
 }
