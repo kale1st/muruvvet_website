4e5bcecf2bf9d2b7f463c16b3ce3430fae42c623
azizkale <azizkale@hotmail.com>
2024-05-27 14:52:01 +0200

    adress updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
   data: any = {
-    location: "Am Schlehdorn 10, 37077 Göttingen/Germany",
-    email: "azizkale@hotmail.com",
-    phone: "+49 17657708936"
-  }
+    location: 'Göttingen/Germany',
+    email: 'azizkale@hotmail.com',
+    phone: '+49 17657708936',
+  };
 }
