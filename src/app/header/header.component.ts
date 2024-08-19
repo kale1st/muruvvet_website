751cdd162b6ebcf198a6e744d75ea1e5f4d67362
kaleaziz <aziz.kale@emm-it.de>
2024-08-19 11:48:22 +0200

    second project added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,23 +4,14 @@ import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-
 })
 export class HeaderComponent {
   @ViewChild('header') header: ElementRef | any;
   @ViewChild('body') body: ElementRef | any;
 
-  isActive: boolean = true;//side menu active?
+  isActive: boolean = true; //side menu active?
 
-  constructor(private renderer: Renderer2) { }
-
-  // <div #body class="mobile-nav-active">
-  // <i class="bi mobile-nav-toggle d-lg-none bi-x"></i>
-
-  //   <i
-  //   class="bi mobile-nav-toggle d-lg-none bi-list"
-  //   (click)="toggleMobileNav()"
-  // ></i>
+  constructor(private renderer: Renderer2) {}
 
   toggleMobileNav(): void {
     if (this.isActive) {
@@ -28,13 +19,11 @@ export class HeaderComponent {
       this.renderer.addClass(this.header.nativeElement, 'bi-x');
       this.renderer.addClass(this.body.nativeElement, 'mobile-nav-active');
       this.isActive = false;
-    }
-    else {
+    } else {
       this.renderer.removeClass(this.header.nativeElement, 'bi-x');
       this.renderer.addClass(this.header.nativeElement, 'bi-list');
       this.renderer.removeClass(this.body.nativeElement, 'mobile-nav-active');
       this.isActive = true;
     }
   }
-
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,8 +12,6 @@ export class ProjectsComponent implements OnInit {
     {
       name: `Pilot Training Management Application for Aero Bildung LLC`,
       description: `
-        Here’s a revised version of your project description that emphasizes your personal contributions, including your coding work with Java, Hibernate, Spring Boot, and Angular:
-
         As the Team Lead at <a href="https://emm-it.de" target="_blank">EMM-IT</a>, I am leading the development of a software application for  <a href="https://aero-bildung.de/de/ueber-aero-bildung/" target="_blank">Aero Bildung</a>, designed to manage the training courses for pilot candidates. This application enables the efficient organization and tracking of courses undertaken by pilots, including the management of certification details. My role extends beyond leadership; I am deeply involved in the hands-on development of the application.
 
         I personally contribute to both the backend and frontend aspects of the project, coding in Java with Hibernate and Spring Boot to build a robust and scalable backend. On the frontend, I utilize Angular to create a dynamic and responsive user interface. Additionally, I work with MSSQL for database management, ensuring accurate and reliable record-keeping of certifications and other essential data.
@@ -23,10 +21,10 @@ export class ProjectsComponent implements OnInit {
       image: 'assets/img/projects/aero.png',
     },
     {
-      name: 'Project Two',
-      description: 'A brief description of Project Two.',
-      link: 'https://github.com/yourusername/project-two',
-      image: 'assets/images/project-two.png',
+      name: 'EMM-IT Website Enhancements and Bilingual Support',
+      description: `I worked on enhancing the <a href="https://emm-it.de/career" target="_blank">EMM-IT</a> website by developing a new "Career" page, which provides information on job opportunities and encourages potential candidates to connect with the company. Using Java Spring Boot and RESTful APIs, I also developed the backend infrastructure from scratch for the site. Additionally, I implemented bilingual support, making the website fully accessible to both English and German-speaking users. These enhancements have significantly improved user experience and accessibility, helping EMM-IT reach a more diverse audience.`,
+      link: 'https://emm-it.de',
+      image: 'assets/img/projects/emmit.png',
     },
     {
       name: 'Project Three',
