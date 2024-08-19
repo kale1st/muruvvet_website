2f748983779240f60fb7af8823d51b26a5bae655
kaleaziz <aziz.kale@emm-it.de>
2024-08-19 09:48:45 +0200

    projects aded to the menu

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -11,7 +11,7 @@ export class ResumeComponent {
   data: any = {
     title: 'Resume',
     description:
-      'As a dynamic and adaptable coder, I thrive on learning and quickly adapting to new challenges. I excel in maintaining precision and focus in coding tasks, finding joy in collaborating with teams. Passionate about the interplay between humans and machines, I bring enthusiasm and teamwork to every project.',
+      'I thrive on learning and quickly adapting to new challenges. I excel in maintaining precision and focus in coding tasks, finding joy in collaborating with teams. Passionate about the interplay between humans and machines, I bring enthusiasm and teamwork to every project.',
   };
 
   downloadResume() {
