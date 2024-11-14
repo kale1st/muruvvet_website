a85af9012e02f683625681c087237ab82df70648
kaleaziz <aziz.kale@emm-it.de>
2024-11-14 14:26:18 +0100

    resume updated

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,11 +12,10 @@ export class ProjectsComponent implements OnInit {
     {
       name: `Pilot Training Management Application for Aero Bildung LLC`,
       description: `
-        As the Team Lead at <a href="https://emm-it.de/" target="_blank">EMM-IT</a>, I am leading the development of a software application for  <a href="https://aero-bildung.de/de/ueber-aero-bildung/" target="_blank">Aero Bildung</a>, designed to manage the training courses for pilot candidates. This application enables the efficient organization and tracking of courses undertaken by pilots, including the management of certification details. My role extends beyond leadership; I am deeply involved in the hands-on development of the application.
+       As a Full Stack Developer at <a href="https://emm-it.de/" target="_blank">EMM-IT</a>, I am leading the development of a software application for  <a href="https://aero-bildung.de/de/ueber-aero-bildung/" target="_blank">Aero Bildung</a>, designed to manage the training courses for pilot candidates. This application enables the efficient organization and tracking of courses undertaken by pilots, including the management of certification details. My role extends beyond leadership; I am deeply involved in the hands-on development of the application.
 
-        I personally contribute to both the backend and frontend aspects of the project, coding in Java with Hibernate and Spring Boot to build a robust and scalable backend. On the frontend, I utilize Angular to create a dynamic and responsive user interface. Additionally, I work with MSSQL for database management, ensuring accurate and reliable record-keeping of certifications and other essential data.
-
-        While I oversee the development process, coordinate the team’s efforts, and ensure successful delivery, I also take pride in being directly involved in the technical implementation. My dual role as both a leader and a developer allows me to drive the project forward effectively, ensuring that the platform not only meets the client’s needs but also reflects high standards of software engineering.`,
+      I personally contribute to both the backend and frontend aspects of the project, coding in Java with Hibernate and Spring Boot to build a robust and scalable backend. On the frontend, I utilize Angular to create a dynamic and responsive user interface. Additionally, I work with MSSQL for database management, ensuring accurate and reliable record-keeping of certifications and other essential data.
+      While I oversee the development process, coordinate the team’s efforts, and ensure successful delivery, I also take pride in being directly involved in the technical implementation. `,
       link: 'https://www.youtube.com/watch?v=sZFY0chh230',
       image: 'assets/img/projects/aero.png',
       githubLink: '',
@@ -32,7 +31,7 @@ export class ProjectsComponent implements OnInit {
         ],
         database: ['MSSQL'],
         devOps: ['Kubernetes (AKS)', 'Github Actions'],
-        otherSkills: ['Team Leadership', 'Code Review'],
+        otherSkills: ['Code Review'],
       },
     },
     {
